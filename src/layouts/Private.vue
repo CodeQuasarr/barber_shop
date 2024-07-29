21f32dd466028238302f1d4f5ddb19423b83181d
Samuel ANYARONKE <dsanyaronke@gmail.com>
2024-07-29 02:17:39 +0200

    init de la dashboard

---
+++ src/layouts/Private.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="py-24 container mx-auto">
+        fdggfdgdfgdfgdfgd
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
