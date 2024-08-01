b1b957b5e34576a13bab6798a96ac03e0032f948
Samuel ANYARONKE <dsanyaronke@gmail.com>
2024-08-01 13:06:28 +0200

    V0

--- src/views/protected/DashboardView.vue
+++ src/views/protected/DashboardView.vue
@@ -2,7 +2,6 @@
 
 import Private from "@/layouts/Private.vue";
 import {computed, ref} from "vue";
-import {useUserStore} from "../../stores/user";
 
 const reservations = ref([
     { id: 1, date: '2023-10-01', time: '10:00', description: 'Réservation 1', isUpcoming: true },
@@ -50,14 +49,35 @@ const filteredReservations = computed(() => {
 const filterReservations = () => {
     // This method is triggered when the filter changes
 };
+
+
+const tester = async () => {
+    const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/orders/${1}/invoice`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer 2|CA4usmhyRNjsCMtdDxZtBZKrOvuNJvd4QIHI2GPe1dc2523e`,
+        },
+    });
+
+    const blob = await response.blob();
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement('a');
+
+    link.href = url;
+    link.setAttribute('download', `invoice_${1}.pdf`);
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+    return;
+};
 </script>
 
 <template>
     <div>
         <Private>
             <div class="py-24">
-              dfgdfg
-              {{ useUserStore().getToken }}
+                <button @click="tester">tester</button>
                 <h1 class="text-4xl font-semibold text-gray-800">Dashboard</h1>
                 <p class="mt-4 text-gray-600">Welcome to your dashboard</p>
 
